821e974aedeb64ce694a7321c143b5c3fc567529
Vasudevan Nedumpozhimana <vasudevan.nedumpozhimana@adaptcentre.ie>
2021-03-08 16:35:44 +0000

    added BERTopic

--- main.py
+++ main.py
@@ -8,75 +8,54 @@ Created on Thu Feb  4 14:19:49 2021
 
 
 import pickle
+import pandas as pd
+import argparse
+
+from bertopic import BERTopic
 
 from TopicModel import TopicModel
 from Analyser import Analyser
+from Merger import Merger
 import os
 
-import pandas as pd
-
-import argparse
-
-
-def view(Data):    
-    #print(Data)
-    #for i in Data['Dominant_Topic'].unique():
-    #    Data[i]
-    #print(Data.groupby(['Dominant_Topic']).mean())
-    #print(Data.groupby(['Dominant_Topic']).size())
-    #print(Data.groupby(['Dominant_Topic','Labels']).size())    
     
-    #Topics = {}
-    Exps,Labels,Keywords,OldTopics = {},{},{},{}
+def view(Data):        
+    Record = {}
     
     for i,row in Data.iterrows():
-        exp = row['Expressions']
         topic = row['Dominant_Topic']
-        label = row['Labels']
-        kw = row['Keywords']
-        #print(exp)
-        if topic not in Exps:   Exps[topic]=[]
-        if exp not in Exps[topic]:  Exps[topic].append(exp)
-        if topic not in Labels:   Labels[topic]=[]
-        Labels[topic].append(label)
-        if topic not in Keywords:   Keywords[topic]=[kw]
-        elif kw not in Keywords[topic]: Keywords[topic].append(kw)
-        #else:   assert Keywords[topic].append(==kw
-        
-        if topic not in OldTopics:    OldTopics[topic]=[]
-        if row['Old_Topic'] not in OldTopics[topic]:    OldTopics[topic].append(row['Old_Topic'])
-        
-        #if topic not in Topics: Topics[topic]={}
-        #if exp not in Topics[topic]:    Topics[topic][exp]=0
-    #Topics[topic][exp]+=1
-    #print(row['Document_No'],row['Dominant_Topic'],row['Keywords'],row['Text'])
-    
-    for topic in Exps:
-        ratio = round(sum(Labels[topic])/len(Labels[topic]),4)
-        ratio = (1-ratio) if ratio<0.5 else ratio
-        print('Topic:',int(topic))
-        print('Labels:',sum(Labels[topic]),len(Labels[topic]),ratio)
-        print('Expressions:',', '.join(Exps[topic]))
-        for k in Keywords[topic]:   print('Keywords:', k)
-        print('Old Topics:',OldTopics[topic])
+        oldtopic = row['Old_Topic'] if 'Old_Topic' in row else topic
 
-    '''
-    for topic in Topics:
-        print(str(int(topic))+' - '+','.join([exp+':'+str(Topics[topic][exp]) for exp in Topics[topic]]))
-    topic_count = [sum([Topics[topic][exp] for exp in Topics[topic]]) for topic in Topics]
-    print(topic_count)
-    '''
-    
-    '''
-    for topic in Topics:
-        print(str(int(topic))+' - '+','.join([exp+'('+str(Topics[topic][exp][0])+','+str(Topics[topic][exp][1])+')' for exp in Topics[topic]]))
+        if topic not in Record: 
+            Record[topic]={'Exps':[],'Labels':[],'Keywords':[],'OldTopics':[]}
+        if row['Expressions'] not in Record[topic]['Exps']:  Record[topic]['Exps'].append(row['Expressions'])
+        Record[topic]['Labels'].append(row['Labels'])
+        if row['Keywords'] not in Record[topic]['Keywords']:  Record[topic]['Keywords'].append(row['Keywords'])
+        if oldtopic not in Record[topic]['OldTopics']:  Record[topic]['OldTopics'].append(oldtopic)
         
-    topic_count = [sum([Topics[topic][exp][1]+Topics[topic][exp][1] for exp in Topics[topic]]) for topic in Topics]
-    print(topic_count)
-    '''
+    for topic in sorted(Record.keys()):
+        id_count = sum(Record[topic]['Labels'])
+        lt_count = len(Record[topic]['Labels'])-id_count
+        print('Topic:',topic,lt_count,id_count,id_count/len(Record[topic]['Labels']))
+        print('Old Topics:',Record[topic]['OldTopics'])
+        print('Expressions:',', '.join(Record[topic]['Exps']))
+        for k in Record[topic]['Keywords']:   print('Keywords:', k)
+        print('')
+
+def shortview(Data):
+    data = Data.groupby(['Dominant_Topic','Labels','Old_Topic']).size()
     
-    #union = lambda x: reduce(set.union, x)
-    #print(dom_topics.groupby(['Dominant_Topic']).aggregate({'Expression':union,'Label':'mean'}))
+    #print(data)
+    for dt in Data['Dominant_Topic'].unique():
+        data = Data.loc[Data['Dominant_Topic']==dt]
+        literal = data.groupby('Labels').size()[0]
+        idiom = data.groupby('Labels').size()[1]
+        size = idiom+literal
+        no_topics = data['Old_Topic'].unique()
+        print(dt,size,idiom/size,no_topics)
+
+        
+    #size, ratio, number of topics
     
 def perLabelTopicModeling(no_topics):
     dataset = pickle.load(open("../Data/ID/vnics_dataset_full_ratio-split.pkl", "rb"), encoding='latin1')       
@@ -136,7 +115,7 @@ def trainTopicModel(n,num_topics,use_vnic):
     TM.save()
     print('Trainig finished')
     
-def predictTopic():
+def predictTopic(merge):
     '''read data as a list of sentences'''
     dataset = pickle.load(open("../Data/ID/vnics_dataset_full_ratio-split.pkl", "rb"), encoding='latin1')       
     
@@ -147,11 +126,13 @@ def predictTopic():
     TM = TopicModel()
     TM.load()
     topics = TM.topicModel(data,expressions,labels)
+    if merge:   topics = merge(topics)
+
     #with pd.option_context('display.max_rows', None, 'display.max_columns', None):    print(topics)
     view(topics)
     return topics
         
-def idiomTest():  
+def idiomTest(merge):  
     '''read data as a list of sentences'''
     dataset = pickle.load(open("../Data/ID/vnics_dataset_full_ratio-split.pkl", "rb"), encoding='latin1')       
     
@@ -162,10 +143,57 @@ def idiomTest():
     TM = TopicModel()
     TM.load()
     topics = TM.topicModel(data,expressions,labels)
+    
+    if merge:   
+        M = Merger(m=12)
+        topics = M.Merge(topics)
+        
     A = Analyser(5)
     A.perTopicAnalysis(topics)        
     
     #compute_coherence_values(start=2, limit=100, step=1)
+    
+def BERTTopicAnalysis(min_topic_size):
+    dataset = pickle.load(open("../Data/ID/vnics_dataset_full_ratio-split.pkl", "rb"), encoding='latin1')       
+    
+    data = [p['sent'].replace(' &apos;','\'') for p in dataset["train_sample"]+dataset["test_sample"]]
+    labels = [p['lab_int'] for p in dataset["train_sample"]+dataset["test_sample"]]
+    expressions = [p['verb']+' '+p['noun'] for p in dataset["train_sample"]+dataset["test_sample"]]
+    
+    #topic_model = BERTopic(min_topic_size=8)
+    topic_model = BERTopic(min_topic_size=min_topic_size)
+    #topic_model = BERTopic()
+    topics,_ = topic_model.fit_transform(data)
+    
+    sent = pd.Series(data,name='Sent')
+    exp = pd.Series(expressions,name='Expressions')
+    label = pd.Series(labels,name='Labels')
+    kw = pd.Series([','.join([k for k,_ in topic_model.get_topic(t)]) for t in topics],name='Keywords')
+    dt = pd.Series(topics,name='Dominant_Topic')
+    
+    Topics = pd.concat([dt,kw,sent,label,exp],axis=1)
+    print('Number of samples',len(Topics.index))
+    
+    '''Remove outliers'''
+    Topics = Topics.loc[Topics['Dominant_Topic']!=-1]
+
+    print('Number of Topics:',len(Topics['Dominant_Topic'].unique()))
+    print('Number of samples after outlier removal',len(Topics.index))
+    
+    lc = 100000
+    sc = 1
+    tc = 100
+    #tc = 1000/(len(Topics['Dominant_Topic'].unique())-m+1)
+    
+    M = Merger(lc=lc,sc=sc,tc=tc)
+    A = Analyser(5)
+    for m in range(5,21,5):
+        print('Number of topic groups after merging)',m)
+
+        MergedTopics = M.Merge(Topics,m)
+        A.perTopicAnalysis(MergedTopics) 
+        print('Number of sentences after merging:',len(MergedTopics.index))    
+        #shortview(MergedTopics)
         
     
 if __name__ == '__main__':
@@ -179,12 +207,19 @@ if __name__ == '__main__':
     parser.add_argument('--train', action='store_true',help='Train topic model')
     parser.add_argument('--test', action='store_true',help='Test topic model')
     parser.add_argument('--idiomtest', action='store_true',help='Test topic model')
+
     parser.add_argument('--use_vnic', action='store_true',help='Use VNIC data along with BNC for training')
+    parser.add_argument('--bertopic', action='store_true',help='Test topic model')
+    parser.add_argument('--merge', action='store_true',help='Test topic model')
+    parser.add_argument('--m', type=int, default=10, help='number of topics')
+    parser.add_argument('--min_topic_size', type=int, default=5, help='minimum size of topic')
+
  
     args=parser.parse_args() 
     if args.train:  trainTopicModel(args.n, args.num_topics, args.use_vnic)
-    if args.test:   predictTopic()
-    if args.idiomtest:  idiomTest()
+    if args.test:   predictTopic(args.merge)
+    if args.idiomtest:  idiomTest(args.merge)
+    if args.bertopic:   BERTTopicAnalysis(args.min_topic_size)
 
 
 #TM = TopicModel()
