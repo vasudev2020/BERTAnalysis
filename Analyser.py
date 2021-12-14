f48c3a91de1d521a87939b825942a976df3f2b77
Vasudevan Nedumpozhimana <vasudevan.nedumpozhimana@adaptcentre.ie>
2021-12-14 16:23:02 +0000

    Cleaning

--- Analyser.py
+++ Analyser.py
@@ -5,8 +5,7 @@ Created on Mon Feb  8 17:09:53 2021
 
 @author: vasu
 """
-#from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix,roc_auc_score
-from sklearn.metrics import accuracy_score, f1_score, roc_auc_score
+from sklearn.metrics import roc_auc_score
 from sklearn.model_selection import StratifiedKFold
 from sklearn.neural_network import MLPClassifier
 from scipy import stats
@@ -16,10 +15,7 @@ from sklearn.exceptions import ConvergenceWarning
 
 import random
 
-
-
 from Embedding import Embedding
-
 
 class Analyser:   
     def __init__(self,num_folds,multiclass=False,allembs=False):
@@ -65,7 +61,6 @@ class Analyser:
             with warnings.catch_warnings():
                 filterwarnings("ignore", category=ConvergenceWarning)
                 model.fit(base_trainX,base_trainY)
-                #pred = model.predict(base_testX)
                 if self.multiclass:
                     pred_proba = [proba for proba in model.predict_proba(base_testX)]
                     base_Auc = roc_auc_score(base_testY,pred_proba,multi_class='ovr')
@@ -76,21 +71,12 @@ class Analyser:
             mbs += base_Auc
         mbs = mbs / 10
 
-        Acc,F1,Auc=[],[],[]
-        for tr_topic in range(len(self.Folds)):
-            Acc.append([])
-            F1.append([])
-            Auc.append([])
-            for ts_topic in range(len(self.Folds)):
-                #Acc[tr_topic].append(0)
-                #F1[tr_topic].append(0)
-                Auc[tr_topic].append(0)
+        Auc = [[0 for ts in range(len(self.Folds))] for tr in range(len(self.Folds))]
     
         for tr_topic in range(len(self.Folds)):
             ts_topic = tr_topic
     
             for test_fold in range(self.num_folds):
-                #trainX = [self.bert.getMean(sent) for fold in range(5) for sent in self.Folds[tr_topic][fold]['Sent'] if fold!=test_fold]
                 trainX = [self.getEmb(sent,embtype) for fold in range(self.num_folds) for sent in self.Folds[tr_topic][fold]['Sent'] if fold!=test_fold]
                 trainY = [label for fold in range(self.num_folds) for label in self.Folds[tr_topic][fold]['Labels'] if fold!=test_fold]
                 with warnings.catch_warnings():
@@ -105,21 +91,12 @@ class Analyser:
                         print(ts_topic,test_fold, sum(testY))
                         print(Data.groupby(['Dominant_Topic','Labels']).size()[ts_topic])
                     
-                    #pred = model.predict(testX)
-                    #pred_proba = [proba[1] for proba in model.predict_proba(testX)]
-                    #pred_proba = [proba for proba in model.predict_proba(testX)]
-            
-                    #Acc[tr_topic][ts_topic] += accuracy_score(testY, pred)
-                    #F1[tr_topic][ts_topic] += f1_score(testY, pred, average='binary')
                     if self.multiclass:
-                        #pred_proba = [proba for proba in model.predict_proba(testX)]
                         Auc[tr_topic][ts_topic] += roc_auc_score(testY,model.predict_proba(testX),multi_class='ovr')
                     else:
                         pred_proba = [proba[1] for proba in model.predict_proba(testX)]
                         Auc[tr_topic][ts_topic] += roc_auc_score(testY,pred_proba)
-                    
-                    #Auc[tr_topic][ts_topic] += roc_auc_score(testY,pred_proba,multi_class='ovr')
-         
+                             
         seen_score = [Auc[t][t]/self.num_folds for t in range(len(self.Folds))]
         unseen_score = [(sum(Auc[t]) - Auc[t][t]) / (self.num_folds * (len(self.Folds)-1)) for t in range(len(self.Folds))]
         
